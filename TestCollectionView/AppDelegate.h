404fa20d7474595c1da0bf19455913ba7a4d58e0
Igor Ilyin <ilyin@steelkiwi.com>
2015-12-11 21:29:32 +0200

    Initial Commit

---
+++ TestCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCollectionView
+//
+//  Created by Igor on 12/11/15.
+//  Copyright © 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
